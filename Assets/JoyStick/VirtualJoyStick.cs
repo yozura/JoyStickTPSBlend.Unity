388cd7b2fbf0f1619fd62f88b160a27535b632f9
yozura <savedelove@naver.com>
2020-09-24 11:53:47 +0900

    0924_TPS,JoyStick,Blend

---
+++ Assets/JoyStick/VirtualJoyStick.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems; // 키보드, 마우스, 터치를 이벤트로 보낼 수 있는 기능을 지원
+
+public class VirtualJoyStick : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [SerializeField]
+    private RectTransform lever;
+    private RectTransform rectTransform;
+    
+    [SerializeField, Range(10, 150)]
+    private float leverRange;
+
+    private Vector2 inputDirection;
+    private bool isInput;
+
+    [SerializeField]
+    private TPSCharacterController tps;
+
+    public enum JoyStickType
+    {
+        Move,
+        Rotate,
+    }
+
+    public JoyStickType stickType;
+    public float sensitibity = 1f;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // 드래그를 시작할 때
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        ControlJoyStickLever(eventData);
+        isInput = true;
+    }
+
+    // 드래그 중일 때
+    public void OnDrag(PointerEventData eventData)
+    {
+        ControlJoyStickLever(eventData);
+    }
+
+    // 드래그가 끝났을 때
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        lever.anchoredPosition = Vector2.zero;
+        tps.Move(Vector2.zero);
+        isInput = false;
+    }
+
+    private void ControlJoyStickLever(PointerEventData eventData)
+    {
+        var inputPos = eventData.position - rectTransform.anchoredPosition;
+        var inputVector = inputPos.magnitude < leverRange ? inputPos : inputPos.normalized * leverRange;
+        lever.anchoredPosition = inputVector;
+        inputDirection = inputVector / leverRange;
+    }
+
+    private void InputControlVector()
+    {
+        // 캐릭터에게 입력벡터를 전달
+        // Debug.Log(inputDirection.x + " / " + inputDirection.y);
+        switch(stickType)
+        {
+            case JoyStickType.Move:
+                tps.Move(inputDirection * sensitibity);
+                break;
+            case JoyStickType.Rotate:
+                tps.LookAround(inputDirection * sensitibity);
+                break;
+        }
+
+    }
+
+    private void Update()
+    {
+        if(isInput)
+        {
+            InputControlVector();
+        }
+    }
+}
---
+++ Assets/TPS/TPSCharacterController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class TPSCharacterController : MonoBehaviour
+{
+    [SerializeField] private Transform characterBody;
+    [SerializeField] private Transform cameraArm;
+
+    Animator anim;
+
+    void Start()
+    {
+        anim = characterBody.GetComponent<Animator>();    
+    }
+
+    public void Move(Vector2 inputDirection)
+    {
+        Vector2 moveInput = inputDirection;
+        bool isMove = moveInput.magnitude != 0;
+        // anim.SetBool("isMove", isMove);
+        anim.SetFloat("MoveSpeed", moveInput.magnitude);
+        
+        if(isMove)
+        {
+            Vector3 lookForward = new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized;
+            Vector3 lookRight = new Vector3(cameraArm.right.x, 0f, cameraArm.right.z).normalized;
+            Vector3 moveDir = lookForward * moveInput.y + lookRight * moveInput.x;
+
+            characterBody.forward = moveDir;
+            transform.position += moveDir * Time.deltaTime * 5f;
+        }
+       
+        Debug.DrawRay(cameraArm.position, new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized, Color.red);
+    }
+
+    public void LookAround(Vector3 inputDirection)
+    {
+        // Delta = 이전 값과 현재 값의 차이를 나타날때 주로 쓰임
+        Vector2 mouseDelta = inputDirection;
+        Vector3 camAngle = cameraArm.rotation.eulerAngles;
+        float x = camAngle.x - mouseDelta.y;
+
+        // 카메라의 X축 회전을 제한
+        if(x < 180f)
+            x = Mathf.Clamp(x, -1f, 70f);
+        else
+            x = Mathf.Clamp(x, 335f, 361f);
+
+        cameraArm.rotation = Quaternion.Euler(x, camAngle.y + mouseDelta.x, camAngle.z);
+    }
+}
